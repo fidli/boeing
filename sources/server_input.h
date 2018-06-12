ac7d49954ad9fe0a798a97aea5180e9b18bd53e1
Adam Kubista <kubistaa@gmail.com>
2018-06-12 12:13:41 +0200

    wisddom

---
+++ sources/server_input.h
@@ -0,0 +1,15 @@
+#ifndef SERVER_INPUT
+#define SERVER_INPUT
+
+struct ServerInput{
+    bool boeing1;
+    bool boeing2;
+    
+    bool method1;
+    bool method2;
+    bool method3;
+    
+    bool record;
+};
+
+#endif
